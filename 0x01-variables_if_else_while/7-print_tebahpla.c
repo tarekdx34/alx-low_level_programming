93c5462f5ae73321b191d0e6fbdd0d39d736ecc1
tarek mohamed <tarekdx3@gmail.com>
2023-03-17 14:21:29 -0700

    done

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- *main - print 0123456789 using putchar
+ *main - print alphabet reversed using putchar
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int n = '0';
+	char alpha = 'z';
 
-	while (n <= '9')
+	while (alpha >= 'a')
 	{
-		putchar(n);
-		n++;
+		putchar(alpha);
+		alpha--;
 	}
 	putchar('\n');
 
 	return (0);
-}
+}x
