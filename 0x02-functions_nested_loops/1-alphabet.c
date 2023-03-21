86cdf969d93ba45d16dcbb460d7702c69f759007
tarek mohamed <tarekdx3@gmail.com>
2023-03-21 12:39:12 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char xio= 'a';
+while(xio<='z')
+{
+_putchar(xio);
+xio++;
+}
+return 0;
+}
