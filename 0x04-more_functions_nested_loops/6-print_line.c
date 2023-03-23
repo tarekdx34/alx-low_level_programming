0637ed6dffb8364d752f1d6096618bae54c6f5b6
tarek mohamed <tarekdx3@gmail.com>
2023-03-23 06:21:52 -0700

    done

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,11 +5,11 @@
  */
 void print_line(int n)
 {
-  int i;
-  if (n > 0)
-    {
-      for (i = 0 ; i <= n ; i++)
-	_putchar(' ');
-    }
-  _putchar('\n');
+int i;
+if (n > 0)
+{
+for (i = 0 ; i <= n ; i++)
+_putchar(' ');
+}
+_putchar('\n');
 }
