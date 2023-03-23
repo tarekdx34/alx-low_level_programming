4042b47b0143b05a65aee92584a0a5b1b4cfe9f3
tarek mohamed <tarekdx3@gmail.com>
2023-03-23 06:16:55 -0700

    done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - print the character "_" n times to draw a line
+ *@n : number of times we print "_"
+ */
+void print_line(int n)
+{
+  int i;
+  if (n > 0)
+    {
+      for (i = 0 ; i <= n ; i++)
+	_putchar(' ');
+    }
+  _putchar('\n')
+}
