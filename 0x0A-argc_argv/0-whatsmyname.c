5d2763930b83e180bb3f0a322f5f419ed7b0fd90
tarek mohamed <tarekdx3@gmail.com>
2023-04-10 18:36:24 -0700

    all at once

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,16 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * main - prints the name of the program
- * @argc : argumant counter
- * @argv : argumant array or vector
- * Return : 0
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
  */
-int main (int argc, char *argv[])
+
+int main(int argc, char *argv[])
 {
-printf("%s\n", argv[0]);
-return (0);
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
 }
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+			j *= atoi(argv[i]);
+
+		printf("%d\n", j);
+	}
+
+	return (0);
+}
