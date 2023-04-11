d005b95d295fd341406d13c1b7c0fb61e4da5507
tarek mohamed <tarekdx3@gmail.com>
2023-04-10 18:18:09 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc : argumant counter
+ * @argv : argumant array or vector
+ * Return : 0
+ */
+int main (int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+int main(int agrc, char *agrv[])
+{
+  printf("%d", (agrc-1))
+    return (0);
+}
