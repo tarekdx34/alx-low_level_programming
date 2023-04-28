53d3504e9a7e43be3d74938994e00bd6aa059a9b
tarek mohamed <tarekdx3@gmail.com>
2023-04-28 14:57:24 -0700

    s

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
-void first(void) __attribute__ ((constructor));
-
-void first(void)
+void func(void) __attribute__ ((constructor));
+/**
+ * func - prints a string before main function
+ * Returns: nothing
+*/
+void func(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
